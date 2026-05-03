c9669de3d2d128ee7325bb7cb73e08e2b735bdf8
Chérif <280088034+chenius-fr@users.noreply.github.com>
2026-05-03 14:37:12 +0200

    Test de push manuel

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Terminale PMF - Révisions",
+    pageTitle: "TPMF - Révisions",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
